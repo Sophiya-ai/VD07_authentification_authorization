7f2db9d70bc40892daf1a7515cd1f1d0cad413d6
Sophiya-ai <sophiyabr@gmail.com>
2024-12-13 11:12:59 +0200

    add forms

---
+++ cw_app/forms.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+#EqualTo нужен для того, чтобы сравнивать значения в полях и узнавать, точно ли они одинаковые
+
+#Импортируем модель User из нашего модуля
+from cw_app.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+    #Создаём функции для проверки уникальности имени пользователя и почты
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Такое имя уже существует.') #raise для генерации исключений похожее на try-except
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Такая почта уже используется.')
+
+#Создание класса LoginForm
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Запомни меня') #создание галочки с подписью
+    submit = SubmitField('Login')
